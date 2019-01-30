7723eb337a80c4feb1b59304cdcd7df8a21407b8
gaia <gaiacicalonidesign@gmail.com>
2019-01-30 16:13:50 +0100

    update structure correct

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const x = require("./routes/pizzaRoutes");
--- routes/pizzaRoutes.js
+++ routes/pizzaRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const x = require("../controllers/recipeController.js");
 
+//Get all pizza
 router.get("/", x.list);
 
 module.exports = router;
