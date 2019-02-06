ef482e56161a3c6015af5d9fcca5adc6e2545a30
gaia <gaiacicalonidesign@gmail.com>
2019-02-06 12:06:41 +0100

    gitignore

--- routes/pizzas.js
+++ routes/pizzas.js
@@ -3,19 +3,12 @@ const router = express.Router();
 
 const pizza = require("../controllers/pizzaController.js");
 
-//Get all restaurants
 router.get("/", pizza.list);
-//Create restaurant
 router.get("/create", pizza.create);
-//Save restaurant
 router.post("/save", pizza.save);
-//Get single restaurant by id
 router.get("/show/:id", pizza.show);
-//Edit restaurant
 router.get("/edit/:id", pizza.edit);
-//Update restaurant
 router.post("/update/:id", pizza.update);
-//Delete restaurant
 router.post("/delete/:id", pizza.delete);
 
 //contact2
