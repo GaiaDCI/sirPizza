fb00f5b92a3589fb2a92691ba4d1dceef449c8d9
gaia <gaiacicalonidesign@gmail.com>
2019-02-19 10:56:33 +0100

    authentication fale

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 
 const mongoose = require("mongoose");
 const pizzas = require("./routes/pizzas");
+const User = require("./models/User");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose
   .connect(`mongodb://localhost:27017/sirPizza`, { useNewUrlParser: true })
@@ -22,6 +26,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/pizzas", pizzas);
 app.use("/", pizzas);
+app.use(
+  require("express-session")({
+    secret: "Rusty is the best og in the world",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
--- routes/pizzas.js
+++ routes/pizzas.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const pizza = require("../controllers/pizzaController.js");
 const upload = require("../handler/multer");
+const passport = require("passport");
 
 router.get("/", pizza.home);
 router.get("/list", pizza.list);
@@ -13,5 +14,63 @@ router.get("/show/:id", pizza.show);
 router.get("/edit/:id", pizza.edit);
 router.post("/update/:id", pizza.update);
 router.post("/delete/:id", pizza.delete);
+
+router.get("/secret", isLoggedIn, function(req, res) {
+  res.render("secret");
+});
+
+// Auth Routes
+router.get("/register", function(req, res) {
+  res.render("register");
+});
+
+// Login Routes
+router.get("/login", function(req, res) {
+  res.render("login");
+});
+
+router.post("/register", function(req, res) {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      } //user stragety
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secret"); //once the user sign up
+      });
+    }
+  );
+});
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// middleware
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    res.send("User is " + req.user.id);
+  }
+);
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
